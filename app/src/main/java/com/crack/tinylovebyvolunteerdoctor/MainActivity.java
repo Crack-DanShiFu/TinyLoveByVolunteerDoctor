0fbd9c2aa0b56c09a2d1e0ad7ab5db74b6c0a790
crack <1124241615@qq.com>
2019-03-05 21:05:47 +0800

    2

--- app/src/main/java/com/crack/tinylovebyvolunteerdoctor/MainActivity.java
+++ app/src/main/java/com/crack/tinylovebyvolunteerdoctor/MainActivity.java
@@ -8,15 +8,20 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.crack.adapter.RecommendRecyclerViewAdapter;
+import com.crack.component.GlideImageLoader;
 import com.crack.component.RecommendItem;
 import com.crack.https.CallBackUtil;
 import com.crack.https.OkhttpUtil;
 import com.crack.utils.BaseActivity;
 import com.crack.utils.LogUtils;
-import com.crack.utils.ToastUtil;
+
+import com.yyydjk.library.BannerLayout;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,6 +38,10 @@ import okhttp3.Response;
 public class MainActivity extends BaseActivity {
     @BindView(R.id.recommendRecyclerView)
     public RecyclerView mRecyclerView;
+    @BindView(R.id.banner)
+    public BannerLayout banner;
+
+
     private LinearLayoutManager mLinearLayoutManager;
     private List<RecommendItem> mData;
     private RecommendRecyclerViewAdapter recycleViewAdapter;
@@ -43,6 +52,8 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         ButterKnife.bind(this);
+
+        init();
         mData = new ArrayList<>();
         mLinearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
@@ -61,6 +72,17 @@ public class MainActivity extends BaseActivity {
                 LogUtils.d(response);
             }
         });
+    }
+
+
+
+    public void init(){
+        final List<String> urls = new ArrayList<>();
+        urls.add("https://pic.kuaizhan.com/g3/70/61/04e0-1185-4a74-99e1-8f287ab919c540/imageView/v1/thumbnail/640x320");
+        urls.add("https://pic.kuaizhan.com/g3/29/3b/7f2a-9329-4414-a2cf-b94c082d4b5115/imageView/v1/thumbnail/640x360");
+        urls.add("https://pic.kuaizhan.com/g3/79/44/784d-cc37-464a-be5d-b29e82e8ff0d04/imageView/v1/thumbnail/640x360");
+        banner.setImageLoader(new GlideImageLoader());
+        banner.setViewUrls(urls);
     }
 
 }
